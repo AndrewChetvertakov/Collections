a05ef3ea09d3ba94b724f043ec5604e8732b83bc
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2021-07-12 21:54:12 -0400

    Longest Palindromic Substring

--- src/LeetCode/medium_String_tasks/LongestPalindromicSubstringGG.java
+++ src/LeetCode/medium_String_tasks/LongestPalindromicSubstringGG.java
@@ -3,18 +3,14 @@ package LeetCode.medium_String_tasks;
 public class LongestPalindromicSubstringGG {
     public static void main(String[] args) {
         String str = "noonllpewcivic";
-
         System.out.println(returnLongestPalindrome(str));
     }
 
-
     public static String returnLongestPalindrome(String str) {
-
         char[] input = str.toCharArray();
         int[] indexes = new int[2];
 
         for (int i = 0; i < input.length; i++) {
-
             checkPalindrome(input, indexes, i, i);   // odd
             checkPalindrome(input, indexes, i, i+1); //even
         }
@@ -22,7 +18,6 @@ public class LongestPalindromicSubstringGG {
     }
 
     public static void checkPalindrome(char[] input, int[] indexes, int start, int end){
-
         while(start >= 0 && end <= input.length -1){
             if(input[start] != input[end]){break;}
             start--;
@@ -32,12 +27,9 @@ public class LongestPalindromicSubstringGG {
             //     noon  <-4
             //     0123
         }
-
         if(end - start > indexes[1] - indexes[0]){
             indexes[1] = end;
             indexes[0] = start+1;
         }
-
     }
-
 }
---
+++ src/collections/iterate_through_collections/TestPew.java
@@ -0,0 +1,12 @@
+package collections.iterate_through_collections;
+
+import java.util.*;
+
+public class TestPew {
+    public static void main(String[] args) {
+        Iterable<Integer> pew = new TreeSet<>();
+        Collection<Integer> pew2 = new TreeSet<>();
+
+
+    }
+}
