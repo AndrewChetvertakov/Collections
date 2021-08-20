daa45b332587856dc8818e44cd7b63eaa6fdef6a
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2021-08-20 01:25:37 -0400

    Different tasks done

--- src/Tests.java
+++ src/Tests.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -5,12 +6,37 @@ import java.util.stream.Collectors;
 public class Tests {
     public static void main(String[] args) {
 
-       String[] pew = {"whyy", "pew", "scrub", "whaty", "Jamal"};
-        List<String> result = Arrays.stream(pew)
-                .map(e -> e+="y")
-                .filter(e -> !e.contains("yy"))
-                .collect(Collectors.toList());
-        System.out.println(result);
+       String[] pew = {" whyy ", " pew ", " scrub      ", " whaty ", " Jamal "};
+//        List<String> result = Arrays.stream(pew)
+//                .map(e -> e+="y")
+//                .filter(e -> !e.contains("yy"))
+//                .collect(Collectors.toList());
+//        System.out.println(result);
+        List<String> gointToBarberer = new ArrayList<>(Arrays.asList(pew));
+        System.out.println(newbiewBarberer(gointToBarberer));
 
+
+
+
+
+//        List<String> shavedList = gointToBarberer.stream().map(String::trim).collect(Collectors.toList() );
+//
+//        System.out.println(shavedList);
     }
+
+    public static List<String> barberer(List<String> gottaShave){
+        if(gottaShave.size() == 0) return null;
+        return gottaShave.stream().map(String::trim).collect(Collectors.toList() );
+    }
+
+    public static List<String> newbiewBarberer(List<String> toBeTrimmed){
+        if(toBeTrimmed.size() == 0) return null;
+        List<String> result = new ArrayList<>(toBeTrimmed.size());
+        for(int i = 0; i< toBeTrimmed.size(); i++){
+            result.add(toBeTrimmed.get(i).trim());
+        }
+        return result;
+    }
+
+
 }
