2b6cfb0e05981ff1b727bb1326df0cd8af0ac8c3
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2021-10-13 02:47:16 -0400

    some recursion practiced

---
+++ src/recursion_practice/Recursive_GreatestCommonDivisor.java
@@ -0,0 +1,15 @@
+package recursion_practice;
+
+public class Recursive_GreatestCommonDivisor {
+    public static void main(String[] args) {
+        int value = find_GCD(8, 12);
+        System.out.println("result = " + value);
+    }
+    public static int find_GCD(int n, int m){
+
+
+        //todo FINISH ME :D :D :D
+
+        return -1;
+    }
+}
