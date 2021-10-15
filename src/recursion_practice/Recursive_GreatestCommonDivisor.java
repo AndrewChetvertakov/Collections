d31b610c7ee99b8ee39055ceb561e979b1ff9ce6
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2021-10-14 21:56:36 -0400

    recursion continued

--- src/recursion_practice/Recursive_GreatestCommonDivisor.java
+++ src/recursion_practice/Recursive_GreatestCommonDivisor.java
@@ -5,11 +5,18 @@ public class Recursive_GreatestCommonDivisor {
         int value = find_GCD(8, 12);
         System.out.println("result = " + value);
     }
+
+    /*
+        Euclidean algorithm:
+        48/18 = 2 remained 12
+        18/12 = 1 remainder 6
+        12/6 = 2 remainder 0
+         */
+
     public static int find_GCD(int n, int m){
-
-
-        //todo FINISH ME :D :D :D
-
-        return -1;
+        if(n < 0 || m < 0) return -1;
+        if(m == 0) return n;
+        return find_GCD(m, m % n);
     }
+
 }
