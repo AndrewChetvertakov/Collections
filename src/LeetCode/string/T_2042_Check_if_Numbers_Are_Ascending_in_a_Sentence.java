b1ce8bf94a7e97d3e08be155865ebf5db07efa89
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2022-01-29 22:47:01 -0500

    Trees and recursion and some

--- src/LeetCode/string/T_2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.java
+++ src/LeetCode/string/T_2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.java
@@ -1,6 +1,6 @@
 package LeetCode.string;
 
-public interface T_2042_Check_if_Numbers_Are_Ascending_in_a_Sentence {
+public class T_2042_Check_if_Numbers_Are_Ascending_in_a_Sentence {
     public static void main(String[] args) {
 
         String abc = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
