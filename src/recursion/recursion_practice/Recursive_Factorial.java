64eaaa2c88a83da752ec21d12b44594a984ae48c
Andrew Chetvertakov <Ndrewchetvertakov@gmail.com>
2022-02-22 03:21:23 -0500

    [pew]

--- src/recursion/recursion_practice/Recursive_Factorial.java
+++ src/recursion/recursion_practice/Recursive_Factorial.java
@@ -1,10 +1,14 @@
 package recursion.recursion_practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Recursive_Factorial {
     public static void main(String[] args) {
 
         int value = find_factorial_recursively(6);
-        System.out.println("final result = "+value);
+        System.out.println("final result = " + value);
+        System.out.println("dynamic progr = " + find_factorial_dynamicProgramming(6));
 
     }
 
@@ -14,4 +18,18 @@ public class Recursive_Factorial {
         return n * find_factorial_recursively(n-1);
     }
 
+    public static int find_factorial_dynamicProgramming (int n) {
+        if( n < 0) return -1;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(1, 1);
+        return fact(map, n);
+    }
+
+    private static int fact(Map<Integer, Integer> map, int n) {
+        if(map.containsKey(n)) return map.get(n);
+        else {
+            map.put(n, find_factorial_recursively(n-1) * n);
+            return map.get(n);
+        }
+    }
 }
